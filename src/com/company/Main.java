7204de7fec6ae2e4f71bba1ebe865608691157aa
ed633 <edendecolli@icloud.com>
2018-09-11 11:22:23 -0400

    rt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReader = new Scanner(System.in);
+        System.out.println("Do you want to add, subtract, divide, or multiply?");
+        String choose = kbReader.nextLine();
+
     }
 }
